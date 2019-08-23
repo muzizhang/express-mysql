eda2d85331ac29a540494aa8754ffb1113685664
z100068 <z100068@beiruosi.com>
2019-08-23 18:47:39 +0800

    fix error

--- dao/userSqlMap.js
+++ dao/userSqlMap.js
@@ -1,6 +1,6 @@
 var userSqlMap = {
   register: 'insert into user(userName, pwd) values(?, ?)',
-  add: 'insert into user(userName, pwd) values(?, ?)',
+  add: 'select * from user where username = ? and password = ?',
   deleteById: 'delete from users where id = ?',
   update: 'update users set username=?, password=? where id=?',
   list: 'select * from users',
